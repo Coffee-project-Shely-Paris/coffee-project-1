03c3f6e6ab3d68e73d98a17e04618f02d1299c75
Paris Tyus <Tyus.Paris1@gmail.com>
2019-12-18 20:27:17 -0600

    got everything corrected and to shelbys last push

--- main.js
+++ main.js
@@ -1,114 +1,49 @@
-"use strict";
-
-var coffeeList = document.querySelector('#coffees');
-var submitButton = document.querySelector('#submit');
-var roastSelection = document.querySelector('#roast-selection');
-var userInput = document.querySelector('#search-bar');
-var searchSubmit = document.querySelector('#search-btn');
-
-coffeeList.innerHTML = renderCoffees(coffees);
-
-submitButton.addEventListener('click', updateCoffees);
-searchSubmit.addEventListener("click", searchCoffees);
-userInput.addEventListener("keyup", searchCoffees);
-
-var coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
-];
-
-
-function renderCoffee(coffee) {//sets up table//
-    var html = '<div class="coffee">';
-    html += '<h1>' + coffee.name + '</h1>';
-    html += '<p>' + coffee.roast + '</p>';
-    html += '</div>';
-
-    return html
-}
-
-<<<<<<< HEAD
-
-function renderCoffees() {
-=======
-function renderCoffees(filteredList) {
->>>>>>> 7024bba457e744b7d6839de3e43379f1d1fbe724
-    var html = '';
-    for (var i = filteredList.length - 1; i >= 0; i--) {
-        html += renderCoffee(filteredList[i]);
+ "use strict";
+    function renderCoffee(coffee) {//sets up table//
+        var html = '<div class="coffee">';
+        html += '<h1>' + coffee.name + '</h1>';
+        html += '<p>' + coffee.roast + '</p>';
+        html += '</div>';
+        return html
     }
-    return html;
-}
 
+// coffees.sort(function(a, b){return a.id-b.id});
 
-
-function updateCoffees(e) {
-    e.preventDefault(); // don't submit the form, we just want to update the data
-    var selectedRoast = roastSelection.value;
-    var filteredCoffees = [];
-    coffees.forEach(function(coffee) {
-        if (coffee.roast === selectedRoast) {
-            filteredCoffees.push(coffee);
-        }
-    });
-    // tbody.innerHTML = renderCoffees(filteredCoffees);
-    // coffeeList.innerHTML = renderCoffees(filteredCoffees);
-}
-
-function searchCoffees(e) {
-    e.preventDefault(); // don't submit the form, we just want to update the data
-    var searchInput = userInput.value.toLowerCase();
-    var filteredCoffees = [];
-    coffees.forEach(function (coffee) {
-        var currentRoast = coffee.roast.toLowerCase();
-        var currentName = coffee.name.toLowerCase();
-        if (currentRoast.includes(searchInput) || currentName.includes(searchInput)) {
-            filteredCoffees.push(coffee);
-        }
-    });
-    // coffeeList.innerHTML = renderCoffees(filteredCoffees);
-}
+    function renderCoffees() {
+        function renderCoffees(filteredList) {
+            var html = '';
+            for (var i = coffees.length - 1; i >= 0; i--) {
+                html += renderCoffee(coffees[i]);
+                for (var i = filteredList.length - 1; i >= 0; i--) {
+                    html += renderCoffee(filteredList[i]);
+                }
+                return html;
+            }
+        @@ -50,13 +48,13 @@ function searchCoffees(e) {
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-<<<<<<< HEAD
-=======
-var coffees = [
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
-];
->>>>>>> 7024bba457e744b7d6839de3e43379f1d1fbe724
+            var coffees = [
+                {id: 1, name: 'Light City', roast: 'light'},
+                {id: 2, name: 'Half City', roast: 'light'},
+                {id: 3, name: 'Cinnamon', roast: 'light'},
+                {id: 4, name: 'City', roast: 'medium'},
+                {id: 5, name: 'American', roast: 'medium'},
+                {id: 6, name: 'Breakfast', roast: 'medium'},
+                {id: 7, name: 'High', roast: 'dark'},
+                {id: 1, name: 'Light City', roast: 'light'},
+                {id: 8, name: 'Continental', roast: 'dark'},
+                {id: 9, name: 'New Orleans', roast: 'dark'},
+                {id: 10, name: 'European', roast: 'dark'},
+                @@ -74,16 +72,8 @@ var userInput = document.querySelector('#search-bar');
+            var searchSubmit = document.querySelector('#search-btn');
 
 
+            coffeeList.innerHTML = renderCoffees(coffees);
+            coffeeList.innerHTML = renderCoffees(coffees.sort(function(a, b){return a.id-b.id}));
 
-
-<<<<<<< HEAD
-
-
-
+            submitButton.addEventListener('click', updateCoffees);
+            searchSubmit.addEventListener("click", searchCoffees);
+            userInput.addEventListener("keyup", searchCoffees);
 
 
 
@@ -117,10 +52,4 @@ var coffees = [
 
 
 
-=======
-coffeeList.innerHTML = renderCoffees(coffees.sort(function(a, b){return a.id-b.id}));
-
-submitButton.addEventListener('click', updateCoffees);
-searchSubmit.addEventListener("click", searchCoffees);
-userInput.addEventListener("keyup", searchCoffees);
->>>>>>> 7024bba457e744b7d6839de3e43379f1d1fbe724
+            userInput.addEventListener("keyup", searchCoffees)
